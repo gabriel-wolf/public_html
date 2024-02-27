7986d6b22c4c849117270162a9be3049f32df9ed
Gabriel Wolf <gabeericwolf@gmail.com>
2024-02-27 16:13:42 -0500

    cpu status v1 done

---
+++ cpu/server_info.php
@@ -0,0 +1,28 @@
+<?php
+// Get CPU usage
+exec("grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'", $cpuUsage);
+
+// Get total memory and free memory
+exec("free -b", $output);
+list($totalMemory, $usedMemory, $freeMemory, $sharedMemory, $bufferMemory, $availableMemory) = explode(" ", preg_replace('/\s+/', ' ', $output[1]));
+
+// Calculate memory usage percentage
+if ($totalMemory > 0) {
+    $memoryUsage = ($usedMemory / $totalMemory) * 100;
+} else {
+    $memoryUsage = 0; // or any default value
+}
+
+// Convert memory to GB and format it
+$memoryGB = number_format($usedMemory / (1024 * 1024 * 1024), 1);
+
+// Send JSON response
+$json = json_encode(['cpuUsage' => $cpuUsage[0], 'memoryUsage' => $memoryUsage, 'memoryGB' => $memoryGB], JSON_NUMERIC_CHECK);
+
+if ($json === false) {
+    echo 'Json encoding failed: ' . json_last_error_msg();
+} else {
+    header('Content-Type: application/json');
+    echo $json;
+}
+?>
